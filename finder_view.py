ba18b99bc23b2cb7318ac2350270824bdbd3cbfa
nyghtowl <melanie_warrick@yahoo.com>
2013-04-18 17:07:57 -0700

    Revamped weather images as dicationary in a function and setup validation to confirmm daytime.

--- finder_view.py
+++ finder_view.py
@@ -9,7 +9,6 @@ TO DO:
 	Currently small sample used and picking center of neighborhood. Future would be good to find a better way to apply
 
 QUESITONS / ERROR:
-	check about how the seed file is linked to model file - how it calls it when it runs
 	How to search and extroapolate just one word in a string of words
 
 """
@@ -23,7 +22,9 @@ import sun_model
 import requests
 # expect to need for pulling api key from environment
 import os
-
+# leverage for reporting time result
+import datetime
+import time
 
 # initialize program to be a Flask app and set a key to keep client side session secure
 app = Flask(__name__)
@@ -44,8 +45,10 @@ def index():
 def display_search():
 	return render_template('search.html')
 
-# capture forecast from forecast.io
-def forecast(lat,lon):
+# submit lat, long and api key and store json/dicationary result into variable
+def get_forecast(location):
+    lat = location.lati
+    lon = location.longi
     url="https://api.forecast.io/forecast/%s/%f,%f"
 	# pull API key from env with FIO_KEY
     final_url=url%(FIO_KEY, lat,lon)
@@ -54,55 +57,76 @@ def forecast(lat,lon):
     return response.json()
 
 # convert icon result to an image
-def weather_pic(icon):
+def w_pic(icon):
 	pic_location = "/static/img/"
 	# holds weather images for reference
-	weather_pics = {"clear-day":"sun_samp2.jpeg", "rain":"rain.png" , "snow":"snow.png", "sleet":"sleet2.png", "fog":"foggy2.png" , "cloudy":"cloudy.png", "partly-cloudy-day":"partly_cloudy.png"}
+	weather_pics = {
+		"clear-day":"sun_samp2.jpeg", 
+		"rain":"rain.png" , 
+		"snow":"snow.png", 
+		"sleet":"sleet2.png", 
+		"fog":"foggy2.png" , 
+		"cloudy":"cloudy.png", 
+		"partly-cloudy-day":"partly_cloudy.png"
+	}
 	
-	#print weather_pics["clear-day"]
 	# FIX how to handle wind icon result - determine percent cloud cover? and 
+	
 	if icon in weather_pics:
 		#print weather_pics[icon]
 		final_pic_loc = pic_location + weather_pics[icon]
 		print final_pic_loc
 	
-#	print type(icon)
+	#FIX - what happends if not result
+
 	return final_pic_loc
 
-# create actual search function to enter the name of the location
+# pulls forecast information from work on incorporating as_of
+def validate_day(forecast_info):
+	
+	print forecast_info['hourly']['icon']
+	
+	# get actual time and compare on whether it is day
+	ts = int(time.time())
+	sunrise_ts = int(forecast_info['daily']['data'][0]['sunriseTime'])
+	sunset_ts = int(forecast_info['daily']['data'][0]['sunsetTime'])
+
+	
+	if sunrise_ts < ts & ts < sunset_ts:
+		# based on icon result, return a corresponding image
+		return {'pic': w_pic(forecast_info['hourly']['icon'])}
+	else:
+		# FIX print a result if not daytime in that timezone
+		print "it's not daytime"
+
+
+# create search function 
 @app.route('/search', methods=['POST'])
 def search():
 	# capture the query request from the form into a variable
 	question = request.form['query']
+
+	# FIX - way to pull the time from the form or default to the current time - need to add date time
+	# as_of = request.form.get('time', datetime.now())
 
 	# query data model file to match name of location to lat & long and then assign to variables
 	loc_match = db_session.query(Location).filter(Location.n_hood.ilike("%" + question + "%")).one()
 	
 	# confirm the infromation captured matches db; otherwise throw error and ask to search again 
 	if loc_match:
-		lat = loc_match.lati
-		lon = loc_match.longi
-		# submit lat, long and api key and store json/dicationary result into variable
-		forecast_result = forecast(lat, lon)
-		print forecast_result['hourly']['icon']
-		# return the results template
-		icon_result = weather_pic(forecast_result['hourly']['icon']) 
-		
+		# if there is a match the pass to get forecast, validate its day and then get elements to pop results
+		forecast_result = validate_day(get_forecast(loc_match))['pic']
 		#return redirect(url_for('fast_result'), result=forecast_result)
-		return render_template('fast_result.html', result=icon_result)
+		return render_template('fast_result.html', result=forecast_result)
 	else:
+		# FIX using flash or a result on the html page...
 		print "Sorry, we are not covering that area at this time. Please try again."
 		return redirect(url_for('search'))
 
 
-	# parse json result and pull icon and tempurature data and assign to variables
-
-	# based on icon result, return a corresponding image
-
-	# print image and tempurature data on page
+	# FIX add image and tempurature to a dictionary that is passed to page
 
 	
-
 # create view that will show simple sun result from search
 # @app.route('/fast_result')
 # def fast_result():
