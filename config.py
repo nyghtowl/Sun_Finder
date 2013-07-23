046b0cb12cbc3c1db2286756eb8f7db2de97b534
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 11:48:48 -0700

    Imported referenced method from sqlalchemy.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy import create_engine
 
 # pull api keys from environment
 G_KEY = os.environ.get('G_KEY')
@@ -7,5 +8,5 @@ WUI_KEY = os.environ.get('WUI_KEY')
 
 # code to setup a postgres database
 database_url = os.getenv('HEROKU_POSTGRESQL_GOLD_URL', 'postgresql://localhost/app/sun_finder_db')
-engine = db.create_engine(database_url, echo=True) # Shows translations till put to False
+engine = create_engine(database_url, echo=True) # Shows translations till put to False
 
