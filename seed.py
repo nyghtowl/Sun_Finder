39ff123cb296d697b89b1c6e5f85b447162a2791
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 12:05:48 -0700

    Adjusted variable name for clarity.

--- seed.py
+++ seed.py
@@ -7,7 +7,7 @@ Go Live: Add this and database info to gitignore
 from app import models
 import csv
 
-def load_location(session):
+def load_location(db_session):
 	# open file as an object
 	loc_file = open('data/location.txt')
 	# split based on space or tab into a reader object containing lists of strings for each row
@@ -15,17 +15,17 @@ def load_location(session):
 	# pull each row and assign and store based on data labels 
 	for row in loc_obj:
 		store_content = models.Location(id=row[0], lat=row[1], lng=row[2], rad=row[3], n_hood=row[4])
-		# adds the content to the db session
-		session.add(store_content)
+		# adds the content to the db db_session
+		db_session.add(store_content)
 
 
 # seeds the databased with the data
-def main(session):
-	load_location(session)
+def main(db_session):
+	load_location(db_session)
 	# commits changes to the db
-	session.commit()
+	db_session.commit()
 
 # runs if seed is loaded with python first
 if __name__ == "__main__":
-	session = models.connect()
-	main(session)
+	db_session = models.connect()
+	main(db_session)
