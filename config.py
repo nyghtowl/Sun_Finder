ff3f1afdb706b5a5ee6346aebc20479fe4af0720
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 12:00:26 -0700

    Changed model reference.

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ FIO_KEY = os.environ.get('FIO_KEY')
 WUI_KEY = os.environ.get('WUI_KEY')
 
 # code to setup a postgres database
-database_url = os.getenv('HEROKU_POSTGRESQL_GOLD_URL', 'postgresql://localhost/app/sun_finder_db')
+database_url = os.getenv('HEROKU_POSTGRESQL_GOLD_URL', 'postgresql://localhost/sun_model')
 engine = create_engine(database_url, echo=True) # Shows translations till put to False
 
