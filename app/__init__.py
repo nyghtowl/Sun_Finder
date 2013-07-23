d8a4e5c76acd923412057d030841c4b7be46aa94
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 12:31:21 -0700

    Renamed db session generating variables to config and to add clarity.

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,8 @@ import os
 from flask import Flask
 # from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
 from flask.ext.login import LoginManager
-from config import ENGINE
+from config import DB_SESSION
 
 
 # initialize program to be a Flask app and set a key to keep client side session secure
@@ -17,13 +16,9 @@ app.config.from_object('config')
 # variable represents sqlalchemy
 # db = SQLAlchemy(app)
 
-# opens on ongoing session with db
-session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
-
 # class that connects to the declarative_base of sqlalchemy
 Base = declarative_base()
-Base.query = session.query_property()
-
+Base.query = DB_SESSION.query_property()
 
 #login information
 login_manager = LoginManager()
