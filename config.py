bf0f619ee89c59ac85a9aee2bb376b8270eb363d
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 12:31:31 -0700

    Renamed db session generating variables to config and to add clarity.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 # pull api keys from environment
 G_KEY = os.environ.get('G_KEY')
@@ -9,4 +10,7 @@ WUI_KEY = os.environ.get('WUI_KEY')
 # code to setup a postgres database
 database_url = os.getenv('HEROKU_POSTGRESQL_GOLD_URL', 'postgresql://localhost/sun_model')
 ENGINE = create_engine(database_url, echo=True) # Shows translations till put to False
+
+# opens on ongoing session with db
+DB_SESSION = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
 
