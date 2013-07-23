ceaf8c5292e540b019b2cca2977dceb0522b8647
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 15:35:54 -0700

    Moved secret key and added csrf.

--- config.py
+++ config.py
@@ -4,14 +4,21 @@ import os
 
 # Move functions out of model that run model into config
 
-# pull api keys from environment
+# Pull weather api keys
 G_KEY = os.environ.get('G_KEY')
 FIO_KEY = os.environ.get('FIO_KEY')
 WUI_KEY = os.environ.get('WUI_KEY')
 
+SECRET_KEY = os.environ.get('key')
+
+CSRF_ENABLED = True
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# code to setup a postgres database
+# Code to setup a postgres database
 SQLALCHEMY_DB_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://' + os.path.join(basedir, 'sun_model.db'))
 
-   
+# Stores migrate data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_respository')
+
+
