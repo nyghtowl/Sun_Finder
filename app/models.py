982c91cf3c0465ae108863f4ff9288524dfdf4be
nyghtowl <warrick.melanie@gmail.com>
2013-07-23 15:40:41 -0700

    Add back db reference.

--- app/models.py
+++ app/models.py
@@ -6,30 +6,28 @@ applying sqlalchemy
 Go Live: set echo back to False
 
 """
-from app import Base
-from config import DB_SESSION
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from app import db
 import os
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
 # create class and table for users
-class User(Base):
+class User(db.Model):
     __tablename__ = "users"
 
-    # creates user columns, nullable = False means its required
-    id = Column(Integer, primary_key=True)
-    email = Column(String(64), nullable=False)
-    password = Column(String(64), nullable=False)
-    fname = Column(String(64), nullable=False)
-    lname = Column(String(64), nullable=True)
-    mobile = Column(String(15), nullable=True)
-    zipcode = Column(Integer, nullable=True)
+    # creates user db.columns, nullable = False means its required
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    fname = db.Column(db.String(64), nullable=False)
+    lname = db.Column(db.String(64), nullable=True)
+    mobile = db.Column(db.String(15), nullable=True)
+    zipcode = db.Column(db.Integer, nullable=True)
     # accept terms of service
-    accept_tos = Column(Boolean, unique=False, default=True)
+    accept_tos = db.Column(db.Boolean, unique=False, default=True)
     # track when the user created the account
-    timestamp = Column(String(64), nullable=False)
+    timestamp = db.Column(db.String(64), nullable=False)
    
     #methods below for the Flask-login to work
 
@@ -54,14 +52,14 @@ class User(Base):
         return '<User %r' % (self.name)
 
 # create class and table for locations
-class Location(Base):
+class Location(db.Model):
     __tablename__="location"
 
-    id = Column(Integer, primary_key=True)
-    lat = Column(Float) # don't set a limit on size of float - apply to when output
-    lng = Column(Float)
-    rad = Column(Integer)
-    n_hood = Column(String(128))
+    id = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.Float) # don't set a limit on size of db.float - apply to when output
+    lng = db.Column(db.Float)
+    rad = db.Column(db.Integer)
+    n_hood = db.Column(db.String(128))
 
     # FIX - figure out how to create a function out of comparing query txt to list of neighborhoods and return lat & lng
     #def query_match(self, txt):
@@ -73,7 +71,7 @@ class Location(Base):
     def __repr__(self):
         return '<Location %r' % (self.n_hood)
 
-# passes back session for seeding the db
-def connect():
-    # passes back session for seeding the db
-    return DB_SESSION()
+# # passes back session for seeding the db
+# def connect():
+#     # passes back session for seeding the db
+#     return DB_SESSION()
