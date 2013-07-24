d41cc3146ddfc7f84feade421a96eecfe3345d1f
nyghtowl <warrick.melanie@gmail.com>
2013-07-24 11:31:02 -0700

    Finally figured out correct syntax to point to  postgres db and run create db.

--- config.py
+++ config.py
@@ -16,11 +16,13 @@ CSRF_ENABLED = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Code to setup a postgres database
-SUN_MODEL = 'postgresql://' + os.path.join(basedir, 'sun_model_db')
+# SQLALCHEMY_DATABASE_URI = 'postgresql://' + os.path.join(basedir, 'sun_model_db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/sun_finder_db'
+# SUN_MODEL = 'postgresql://' + os.path.join(basedir, 'sun_model_db')
 
 # Code to pull postgres db or local db
 # SQLALCHEMY_DB_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_DATABASE_URI = os.getenv(os.environ['DATABASE_URL'], SUN_MODEL)
+# SQLALCHEMY_DATABASE_URI = os.getenv(os.environ['DATABASE_URL'], SUN_MODEL)
 
 # Stores migrate data files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_respository')
