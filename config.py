71bd5113ec8b5275ff409bca88eb4ad13984e2d1
nyghtowl <warrick.melanie@gmail.com>
2013-07-24 17:38:02 -0700

    Added conditional to check for Heroku database when loaded otherwise use local postgres.

--- config.py
+++ config.py
@@ -16,13 +16,11 @@ CSRF_ENABLED = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Code to setup a postgres database
-# SQLALCHEMY_DATABASE_URI = 'postgresql://' + os.path.join(basedir, 'sun_model_db')
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/sun_finder_db'
-# SUN_MODEL = 'postgresql://' + os.path.join(basedir, 'sun_model_db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/sun_finder_db'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-# Code to pull postgres db or local db
-# SQLALCHEMY_DB_URI = os.environ['DATABASE_URL']
-# SQLALCHEMY_DATABASE_URI = os.getenv(os.environ['DATABASE_URL'], SUN_MODEL)
 
 # Stores migrate data files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
