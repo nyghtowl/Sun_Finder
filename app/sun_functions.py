b519b5cbe190998277e7bf4e7d00cc08c764e660
nyghtowl <warrick.melanie@gmail.com>
2013-07-31 18:24:26 -0700

    Cleaned up comments.

--- app/sun_functions.py
+++ app/sun_functions.py
@@ -6,18 +6,14 @@ Sun Functions
 from app import db as db_session
 from config import G_KEY
 from models import Location, User
-# use requests to pull api info - alternative is urllib - this is more human
-import requests
+import requests # Alt to urllib
 import weather_forecast
-# utilize for regular expressions
-import re
-# leverage for reporting time result
+import re # Regex
 from datetime import datetime, timedelta
-import time
 
 
 def get_coord(txt_query, as_of):
-    # use regex to swap space with plus and add neighborhood to help focus results
+    # Regex to pull space with +
     txt_plus = re.sub('[ ]', '+', txt_query)
 
     # params option not working because location pass is lat,lng - is there a fix?
@@ -37,26 +33,26 @@ def get_coord(txt_query, as_of):
     central_lng = -122.4429
     central_rad = 5000
 
-    # url to pass to Google Places api
+    # Google Places api url
     url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&location=%f,%f&radius=%f&sensor=%s&key=%s"
     
-    #change when ready to cover all bay area #central_rad = 8000
+    # FIX change when ready to cover all bay area #central_rad = 8000
 
-    # request results from Google Places
+    # Results from Google Places
     final_url = url % (txt_plus, central_lat, central_lng, central_rad, 'false', G_KEY)
     response = requests.get(final_url)
     
-    print final_url #test
+    print final_url # Review
 
-    # revise response to json, and seperate out the lat and long
+    # Revise response to json, and seperate out the lat and long
     place_result = response.json()
     result_path = place_result['results'][0]['geometry']['location'] 
     g_lat = result_path['lat']
     g_lng = result_path['lng']
    
-    print g_lat, g_lng # test
+    print g_lat, g_lng # Review
 
-    # return Weather object if coordinates exist
+    # Return Weather object if coordinates exist
     if g_lat:
         return weather_forecast.Weather.get_forecast(g_lat, g_lng, as_of)
     else:
@@ -65,7 +61,7 @@ def get_coord(txt_query, as_of):
         #FIX flash a message to try search again if coord_result is not valid
 
 
-# validate the date chosen for search has data results
+# Validate the date chosen for search has data results
 def validate_date(as_of):
     if as_of.date() < datetime.now().date():
         return False
@@ -73,36 +69,36 @@ def validate_date(as_of):
         return False
     return True
 
-# generate valid as_of date to create weather object
+# Generate valid as_of date to create weather object
 def extract_as_of(date_string):
     if not(date_string):
         as_of = datetime.now()
     else:
-        #grabs date that is entered and combines with automatically generated time
+        # Adds automatically generated time to entered date
+        # FIX - allow to change if allowing time choice
         as_of_date = datetime.strptime(date_string, "%Y-%m-%d")
-        as_of_time = datetime.now().time() #this applies a auto time to the date picked / not timepicker
+        as_of_time = datetime.now().time() # Applies a auto time to the date picked / not timepicker
         as_of = datetime.combine(as_of_date,as_of_time)
 
+    # Fall back to current for bad date
+    # FIX flash an error instead?
     if not validate_date(as_of):
-        # fall back to current for bad date
-        # TODO: flash an error instead?
         print "FAILED date unsupported %s" % as_of
         as_of = datetime.now()
 
     return as_of 
 
-# function to generate search results for the different views
+# Search results
 def search_results(locations, date, txt_query):
 
-    g_lat = None
-    g_lng = None
+    g_lat, g_lng = None
 
-    # determine date captured to utilize
+    # Format date
     as_of = extract_as_of(date)
     
     # FIX - search by specif time
 
-    # utilize local db for coordinates if query relates
+    # Use local db for coordinates if query matches local db
     for location in locations:
         if location.n_hood == txt_query:
             g_lat = location.lat
@@ -110,15 +106,15 @@ def search_results(locations, date, txt_query):
             loc_name = location.n_hood
     if g_lat:
         forecast_result = weather_forecast.Weather.get_forecast(g_lat, g_lng, as_of)
-        forecast_result.add_name(loc_name) # applies neighborhood name if from local db
-    # if no query match to local db use Google Places
+        forecast_result.add_name(loc_name) # Applies neighborhood name if from local db
+    # Use Google Places for coordinates if no query match to local db
     else:
         forecast_result = get_coord(txt_query, as_of)
 
-    # validate time of date to determine picture to assign
+    # Validate time of date to determine picture to assign
     forecast_result.apply_pic()
 
-    # returns forecast result
+    # Return forecast result
     return forecast_result
 
 
