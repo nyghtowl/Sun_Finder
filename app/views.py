0e6f8b770010ab24860373389a52bf90fe365fc7
nyghtowl <warrick.melanie@gmail.com>
2013-08-04 23:12:33 -0700

    Added in g method.

--- app/views.py
+++ app/views.py
@@ -34,7 +34,7 @@ TO DO:
 
 """
 
-from flask import render_template, flash, redirect, session, url_for, request, jsonify
+from flask import render_template, flash, redirect, session, url_for, request, jsonify, g
 from app import db, app, login_manager
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from models import Location, User
