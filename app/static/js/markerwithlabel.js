5dcbb569c6a3b7bd3a47fcc76353cc7994b40584
nyghtowl <warrick.melanie@gmail.com>
2013-08-29 21:02:27 -0500

    Resolve error about insecure content by adding https to markerwithlabel.js. At some point the fixes to that file will need to be moved into the project docs and out of the package.

--- app/static/js/markerwithlabel.js
+++ app/static/js/markerwithlabel.js
@@ -553,7 +553,7 @@ function MarkerWithLabel(opt_options) {
 // added img marker customization to change the image from red to black and make it∂¡
 var img_marker = {
   scaledSize:new google.maps.Size(5, 10),
-  url:"http://labs.google.com/ridefinder/images/mm_20_black.png"
+  url:"https://labs.google.com/ridefinder/images/mm_20_black.png"
 }; 
 
 // MarkerWithLabel inherits from <code>Marker</code>:
